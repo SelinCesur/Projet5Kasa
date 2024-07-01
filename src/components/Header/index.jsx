60b37b0597a5eaf745c44499d8851f8df212f6ed
Selin CESUR <cakmakciselin@gmail.com>
2024-07-01 22:33:49 +0200

    En cour

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,21 @@
 import '../../Styles/Header.scss'
+import logo from '../../Assets/Kasa.png'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <img src={logo} alt="Logo Kasa" />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>
+          </li>
+          <li>
+            <Link to="/a-propos">A propos</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
---
+++ src/pages/APropos/index.jsx
@@ -0,0 +1,14 @@
+function APropos() {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <p>
+        Kasa est une plateforme de location de logements de particulier à
+        particulier. Elle a été créée en 2019 par une équipe de quatre
+        personnes.
+      </p>
+    </div>
+  )
+}
+
+export default APropos
