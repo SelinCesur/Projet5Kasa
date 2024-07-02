a48c002c94ddb10380fc1338a319b8e007cc3124
Selin CESUR <cakmakciselin@gmail.com>
2024-07-02 13:51:01 +0200

    Footer fini

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,16 @@
 import '../../Styles/Footer.scss'
+import logo from '../../Assets/LogoKasaNoir.png'
+
 function Footer() {
   return (
     <footer>
-      <p>Footer</p>
+      <img
+        src={logo}
+        alt="Logo Kasa Noir et Blanc"
+        width={122}
+        height={39.44}
+      />
+      <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   )
 }
