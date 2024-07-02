310aa9e538662cdbc5c31137e3e21c394655bbef
Selin CESUR <cakmakciselin@gmail.com>
2024-07-02 14:40:10 +0200

    Banner fini

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,8 +1,19 @@
 import '../../Styles/Banner.scss'
+import Image1 from '../../Assets/imageSource1.png'
+
 function Banner() {
   return (
-    <div>
-      <h1>My Banner</h1>
+    <div
+      class="banner"
+      style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${Image1})`, // https://stackoverflow.com/questions/4997493/set-opacity-of-background-image-without-affecting-child-elements
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        color: 'black',
+      }}
+    >
+      <h1>Chez vous, partout et ailleurs</h1>
     </div>
   )
 }
