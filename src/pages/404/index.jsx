d9a212433e8ef255d3ad04b7398224c597d221d4
Selin CESUR <cakmakciselin@gmail.com>
2024-07-02 15:27:53 +0200

    Page APropos et 404 en cour

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,10 +1,13 @@
 import Layout from '../../Components/Layout'
+import { Link } from 'react-router-dom'
 
 function Error() {
   return (
     <Layout>
       <div>
-        <h1>Oups 🙈 Cette page n'existe pas</h1>
+        <p> 404</p>
+        <h1>Oups! La page que vous demandez n'existe pas.</h1>
+        <Link to="/">Retourner sur la page d'accueil</Link>
       </div>
     </Layout>
   )
--- src/pages/APropos/index.jsx
+++ src/pages/APropos/index.jsx
@@ -1,13 +1,17 @@
+import Layout from '../../Components/Layout'
+
 function APropos() {
   return (
-    <div>
-      <h1>A propos</h1>
-      <p>
-        Kasa est une plateforme de location de logements de particulier à
-        particulier. Elle a été créée en 2019 par une équipe de quatre
-        personnes.
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>A propos</h1>
+        <p>
+          Kasa est une plateforme de location de logements de particulier à
+          particulier. Elle a été créée en 2019 par une équipe de quatre
+          personnes.
+        </p>
+      </div>
+    </Layout>
   )
 }
 
