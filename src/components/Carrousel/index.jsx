13c62daead823b002910454e4afed9cdeba53450
Selin CESUR <cakmakciselin@gmail.com>
2024-07-04 09:37:20 +0200

    En cour

---
+++ src/components/Carrousel/index.jsx
@@ -0,0 +1,3 @@
+// import VectorFermer from '../../Assets/VectorG.png'
+// import VectorOuvrir from '../../Assets/VectorD.png'
+// import '../../Styles/Carrousel.scss'
---
+++ src/pages/FicheLogement/index.jsx
@@ -0,0 +1,13 @@
+import Layout from '../../Components/Layout'
+import Carrousel from '../../Components/Carrousel'
+import '../../Styles/FicheLogement.scss'
+
+function FicheLogement() {
+  return (
+    <Layout>
+      <Carrousel></Carrousel>
+    </Layout>
+  )
+}
+
+export default FicheLogement
